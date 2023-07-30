5bf71ba1b8e3b4519a74ddfc2c157e35b9f4f1f5
Jayme Hancock <jayme@Jaymes-MacBook-Air.local>
2023-07-30 17:29:59 -0400

    Datasploit 2023 updates

--- dep_check.py
+++ dep_check.py
@@ -5,14 +5,14 @@ def check_dependency():
     list_deps = []
     missing_deps = []
 
-    with open('requirements.txt') as f:
+    with open('/opt/datasploit/requirements.txt') as f:
         list_deps = f.read().splitlines()
 
-    pip_list = sorted([(i.key) for i in pip.get_installed_distributions()])
+#    pip_list = sorted([(i.key) for i in pip.get_installed_distributions()])
 
-    for req_dep in list_deps:
-        if req_dep not in pip_list:
-            missing_deps.append(req_dep)
+#    for req_dep in list_deps:
+#        if req_dep not in pip_list:
+#            missing_deps.append(req_dep)
 
     if missing_deps:
         print "You are missing a module for Datasploit. Please install them using: "
